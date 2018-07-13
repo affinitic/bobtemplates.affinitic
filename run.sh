d0717072c52ccb11a8a7e5548e434facebd0db7a
Sebastien Sirtoli <sebastien.sirtoli@affinitic.be>
2018-07-13 15:14:40 +0200

    Update run to use bobtemplate

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 buildout() { read -p "please provide name of buildout " $1 ; }
-#policy() { read -p "please provide name of policy package " $1 ; }
-#core() { read -p "please provide name of core package " $1 ; }
+policy() { read -p "please provide name of policy package " $1 ; }
+core() { read -p "please provide name of core package " $1 ; }
 theme() { read -p "please provide name of theme package " $1 ; }
 
 buildout buildout_name
@@ -10,8 +10,8 @@ policy policy_name
 theme theme_name
 
 bin/mrbob -O $buildout_name bobtemplates.affinitic:complete_plone5_buildout
-plonecli create addon $core_name
-plonecli create addon $policy_name
-plonecli create addon $theme_name
+bin/mrbob -O $core_name bobtemplates.plone:addon
+bin/mrbob -O $policy_name bobtemplates.plone:addon
+bin/mrbob -O $theme_name bobtemplates.plone:addon
 
 python run.py --policy=$policy_name --core=$core_name --theme=$theme_name
